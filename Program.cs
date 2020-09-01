52055b44af4437130e4884a795f55a437eb229bf
BandersGit <andy.i.flaskan@gmail.com>
2020-09-01 10:51:19 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Filsystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // File.WriteAllText("words.txt", "Thing");
+
+            // string s = File.ReadAllText("words.txt");
+
+            // Console.WriteLine(s);
+
+
+
+
+            // string[] words = {"Hat", "Coat", "Pants"};
+
+            // File.WriteAllLines("words.txt", words);
+
+
+
+            if(File.Exists("words.txt") )
+            {
+            string[] words = File.ReadAllLines("words.txt");
+
+            System.Console.WriteLine(words[0]);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
